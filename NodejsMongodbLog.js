029b0a159e755df5bb911ba8fd3f739c55937ae4
wqh <wqh15381146338@163.com>
2021-12-08 17:13:50 -0800

    add 1

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -2,8 +2,8 @@ const express = require("express")
 const app=express()
 app.use('/',express.static('public'))  //此时默认打开的就是html文件
 app.get("/input",(req,res)=>{
-    res.send(req,query)
-    console.log(req,query)
+    res.send(req.query)
+    console.log(req.query)
 })
 // app.get("/",(req,res)=>{
 //     res.send("hello")
