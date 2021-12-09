5efcb24e0a5a3e4c8b94bd9ebf24dae06db59286
wqh <wqh15381146338@163.com>
2021-12-08 17:33:59 -0800

    add mongoose

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,5 +1,20 @@
 const express = require("express")
 const app=express()
+const mongoose = require('mongoose');   //mongoose
+
+mongoose.connect('mongodb://localhost/test1');  //连接数据库
+const schema={   
+    name:String,
+    age:Number,
+    health:String,
+    hooby:String
+}
+const mydata = mongoose.model('cat1s',schema );  //schema在这里生效
+const kitty = new mydata({ name: 'testZildjian2' });
+kitty.save()
+
+
+
 app.use('/',express.static('public'))  //此时默认打开的就是html文件
 app.get("/input",(req,res)=>{
     res.send(req.query)
