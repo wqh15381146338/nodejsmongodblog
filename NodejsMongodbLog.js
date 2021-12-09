88ba0efbaa915f34266b8b97c1de2994361e0ea3
wqh <wqh15381146338@163.com>
2021-12-08 17:02:15 -0800

    express install successful

---
+++ NodejsMongodbLog.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app=express()
+app.use('/',express.static('public'))  //此时默认打开的就是html文件
+app.get("/input",(req,res)=>{
+    res.end(req,query)
+    console.log(req,query)
+})
+// app.get("/",(req,res)=>{
+//     res.send("hello")
+// })
+app.listen(50526)
